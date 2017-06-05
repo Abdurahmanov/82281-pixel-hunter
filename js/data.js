f2acbee1fb52e90467799be5daa3230353d9fd48
Artur <artur@smoothie.digital>
2017-06-05 11:30:03 +0300

    Replace img

--- js/data.js
+++ js/data.js
@@ -8,18 +8,18 @@ const initialState = Object.freeze({
 const games = Object.freeze({
   games1: {
     title: `Угадайте для каждого изображения фото или рисунок?`,
-    img1: `${img.photos[Math.floor(0 + Math.random() * (2 + 1 - 0))]}`,
-    img2: `${img.paintings[Math.floor(0 + Math.random() * (2 + 1 - 0))]}`,
+    img1: `${img.photos[0]}`,
+    img2: `${img.paintings[2]}`,
   },
   games2: {
     title: `Угадай, фото или рисунок?`,
-    img: `${img.photos[Math.floor(0 + Math.random() * (2 + 1 - 0))]}`,
+    img: `${img.paintings[1]}`,
   },
   games3: {
     title: `Найдите рисунок среди изображений`,
-    img1: `${img.photos[Math.floor(0 + Math.random() * (2 + 1 - 0))]}`,
-    img2: `${img.paintings[Math.floor(0 + Math.random() * (2 + 1 - 0))]}`,
-    img3: `${img.photos[Math.floor(0 + Math.random() * (2 + 1 - 0))]}`,
+    img1: `${img.photos[1]}`,
+    img2: `${img.paintings[0]}`,
+    img3: `${img.photos[2]}`,
   }
 });
 
