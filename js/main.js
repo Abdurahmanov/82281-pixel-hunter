f7354e7a642551f66492df643bb324ad05a21d73
Artur <artur@smoothie.digital>
2017-06-13 12:37:57 +0300

    resize

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ import mainScreen from './intro';
 import addMarkup from './addMarkup';
 
 addMarkup(mainScreen);
+
+import img from './img';
+
+const loadImage = (src, callback) => {
+  const photo = new Image();
+  photo.onload = photo.onerror = (evt) => {
+    callback(evt.type === `error`, photo);
+    photo.onload = photo.onerror;
+  };
+  photo.src = src;
+};
+
+
+loadImage(img.photos[Math.floor(Math.random() * 3)], (err, photo) => {
+  if (err) {
+    // image not loaded
+  } else {
+    // let width = photo.width;
+  }
+});
---
+++ js/resize.js
@@ -0,0 +1,8 @@
+import img from './img';
+
+const resize = {
+  img: img.photos[Math.floor(Math.random() * 3)],
+};
+
+
+export {resize};
